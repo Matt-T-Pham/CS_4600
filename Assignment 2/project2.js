9f5bbc1ef0a256f55ab5a89268304890e9521308
Matt-T-Pham <34947766+Matt-T-Pham@users.noreply.github.com>
2020-09-21 18:07:09 -0600

    FIN
    FIN

--- Assignment 2/project2.js
+++ Assignment 2/project2.js
@@ -4,29 +4,40 @@ let cos = Math.cos;
 
 
 function getDegree(num){
-	return (num * Math.PI / 180)
+	return (num * (Math.PI / 180))
 }
 function rotate(rot){
 	rot = getDegree(rot)
+	// cos -sin 0
+	// sin cos  0
+	// 0   0    1
 	let rotateMatrix = [cos(rot),sin(rot),0,-sin(rot),cos(rot),0,0,0,1]
 	return rotateMatrix
 }
 
 function ApplyScale(scale){
-	let scaleMatrix = [scale,0,0,0,scale,0,0,0,scale]
+	let scaleMatrix = [scale,0,0,0,scale,0,0,0,1]
 	return scaleMatrix
 }
 function trans(positionX, positionY) {
-	let translateMatrix = [0,0,0,0,0,0,positionX,positionY,0]
+	let translateMatrix = [1,0,0,0,1,0,positionX,positionY,1]
 	return translateMatrix;
+}
+function rowTimesColumn(row, col) {
+	let ans = ((row[0] * col[0]) + (row[1] * col[1]) + (row[2] * col[2]))
+	return ans
 }
 function multiplyMatrix(mat1,mat2) {
 	let answerMatrix = [0,0,0,0,0,0,0,0,0]
 
-	for(let i = 0; i < mat1.length; i+=3){
-			answerMatrix[i]= 
-			answerMatrix[i+1]=
-			answerMatrix[i+2]=
+	let mat1Row1 = [mat1[0],mat1[3],mat1[6]]
+	let mat1Row2 = [mat1[1],mat1[4],mat1[7]]
+	let mat1Row3 = [mat1[2],mat1[5],mat1[8]]
+
+	for(let i = 0 ; i < answerMatrix.length; i+=3){
+		answerMatrix[i]  = rowTimesColumn(mat1Row1,[mat2[i],mat2[i+1],mat2[i+2]])
+		answerMatrix[i+1]= rowTimesColumn(mat1Row2,[mat2[i],mat2[i+1],mat2[i+2]])
+		answerMatrix[i+2]= rowTimesColumn(mat1Row3,[mat2[i],mat2[i+1],mat2[i+2]])
 	}
 	return answerMatrix;
 }
@@ -36,16 +47,12 @@ function multiplyMatrix(mat1,mat2) {
 function GetTransform( positionX, positionY, rotation, scale )
 {
 
-	// 1 0 0  |  cos()   sin()  0   |
-	// 0 1 0  |  -sin()  cos()  0   |
-	// 0 0 1  |  0        0     0   |
+	console.log(positionX,positionY)
 	let rotationMatrix = rotate(rotation)
 	let scaleMatrix = ApplyScale(scale)
 	let translateMatrix = trans(positionX,positionY)
-	//rotate * translate * scale
-	let matrixTransform =multiplyMatrix()
-
-	return matrix
+	let ans = multiplyMatrix((multiplyMatrix(translateMatrix,rotationMatrix)),scaleMatrix)
+	return ans;
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
@@ -53,5 +60,6 @@ function GetTransform( positionX, positionY, rotation, scale )
 // The returned transformation first applies trans1 and then trans2.
 function ApplyTransform( trans1, trans2 )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	let ans = multiplyMatrix(trans2,trans1);
+	return ans;
 }
